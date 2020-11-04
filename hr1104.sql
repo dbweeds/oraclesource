87053875116b0bf629f7139da307082c32058fd0
kimjiho <cafe3ye@naver.com>
2020-11-04 16:26:41 +0900

    수정

--- hr1104.sql
+++ hr1104.sql
@@ -1,2 +1,18 @@
-select * form tab;
+--hr 계정이 가지고있는 모든 테이블 보기
 select * from tab;
+
+desc employees;
+
+select * from employees;
+
+select first_name, last_name, job_id from employees;
+
+select distinct job_id from employees;
+
+select last_name, department_id from employees where employee_id = 176;
+
+select last_name, salary from employees where salary >= 12000;
+
+select last_name, salary from employees where salary >= 12000 or salary <= 5000 ;
+
+
